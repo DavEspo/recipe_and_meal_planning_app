05f9c1ff87f35658c5ae0705946de07f5073a663
David <davidrachmaninoff@gmail.com>
2024-10-26 22:31:36 -0400

    Implemented TextField in Recipe Screen

--- lib/test.dart
+++ lib/test.dart
@@ -16,14 +16,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const WelcomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -456,13 +456,13 @@ class RecipeScreen extends State<Recipe> {
               controller: controller,
               decoration: InputDecoration(
                 prefixIcon: const Icon(Icons.search),
-                hintText: 'Find Meal',
+                hintText: 'Book Title',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                   borderSide: const BorderSide(color: Colors.blue)
                 )
               ),
-              onChanged: searchMeal,
+              onChanged: searchBook,
             )
           ),
           Expanded(
@@ -482,7 +482,7 @@ class RecipeScreen extends State<Recipe> {
     );
   }
 
-  void searchMeal(String query) {
+  void searchBook(String query) {
     final suggestions = allMeals.where((Meal) {
       final MealShown = Meal.toLowerCase();
       final input = query.toLowerCase();
@@ -506,6 +506,65 @@ class RecipeScreen extends State<Recipe> {
           );
         },
       ),
+    );
+  }
+}
+
+class MySearchDelegate extends SearchDelegate {
+  List<String> searchResults= [
+    'Brazil',
+    'China',
+    'Mexico',
+    'Venezuela'
+  ];
+  
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+    icon: const Icon(Icons.arrow_back),
+    onPressed: () => close(context, null)
+  );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+    IconButton(
+      icon: Icon(Icons.clear),
+      onPressed: () {
+        if (query.isEmpty) {
+          close(context, null);
+        } else {
+          query = '';
+        }
+      },
+    )
+  ];
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+    child: Text(
+      query,
+      style: TextStyle(fontSize: 64, fontWeight: FontWeight.bold)
+    ),
+  );
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = searchResults.where((searchResult) {
+      final result = searchResult.toLowerCase();
+      final input = query.toLowerCase();
+      return result.contains(input);
+    }).toList();
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {
+            query = suggestion;
+            showResults(context);
+          },
+        );
+      }
     );
   }
 }
